b5d2b62c590259ba633bda16efc9163e33cdf946
krose <krose@tableau.com>
2019-05-23 18:07:51 -0700

    Update popupUrl to use relative path

--- src/DataDrivenParameter.tsx
+++ src/DataDrivenParameter.tsx
@@ -56,8 +56,7 @@ class DataDrivenParameter extends React.Component<any, State> {
 
     // Pops open the configure page
     public configure = (): void => {
-        console.log(`process.env.PUBLIC_URL=${process.env.PUBLIC_URL}`)
-        const popupUrl = (window.location.origin.includes('localhost')) ? `${window.location.origin}/#/config` : `${window.location.origin}/extension-data-driven-parameters/#/config`;
+        const popupUrl = `${window.location.origin}${process.env.PUBLIC_URL}/#/config`;
         const payload = '';
         window.tableau.extensions.ui.displayDialogAsync(popupUrl, payload, { height: 525, width: 450 }).then((closePayload: string) => {
             const settings = window.tableau.extensions.settings.getAll();
